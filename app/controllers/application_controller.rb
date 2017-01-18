e5ecf790471f497026c92a8c893539356e7fdab1
Joe Ryan <Jdryan12@gmail.com>
2017-01-18 13:29:49 -0700

    C update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def goodbye
-      render html: "Goodbye, World!"
-    end
-  def hello
-    render html: "Hola, Mundo!"
+  def courtney
+      render html: "I love Courtney"
     end
 end
