703c1bf63cd0562151c328ee0e864c0e264d2709
Joe Ryan <Jdryan12@gmail.com>
2017-01-18 13:21:37 -0700

    Update Route to Goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  def goodbye
+      render html: "Goodbye, World!"
+    end
   def hello
     render html: "Hola, Mundo!"
-def goodbye
-    render html: "Goodbye, World!"
-  end
+    end
 end
